11517e023ec5342f7e71955156e46e9663aed3e4
Japa Alekhin Llemos <jap@alekhin.io>
2019-10-05 17:41:46 +0800

    added tank component to game render field of grass render tank

--- lib/lame-tank-360.dart
+++ lib/lame-tank-360.dart
@@ -1,14 +1,30 @@
 import 'dart:ui';
 import 'package:flame/game.dart';
+import 'package:lame_tank_360/components/tank.dart';
 
 class LameTank360 extends Game {
   Size screenSize;
+  Tank tank;
 
   @override
   void render(Canvas c) {
     if (screenSize == null) {
       return; // just end execution if there's no screenSize
     }
+
+    // draw a grass field
+    c.drawRect(
+      Rect.fromLTWH(
+        0,
+        0,
+        screenSize.width,
+        screenSize.height,
+      ),
+      Paint()..color = Color(0xff27ae60),
+    );
+
+    // draw the tank
+    tank.render(c);
   }
 
   @override
@@ -21,5 +37,15 @@ class LameTank360 extends Game {
   @override
   void resize(Size size) {
     screenSize = size;
+
+if (tank == null) {
+  tank = Tank(
+    this,
+    position: Offset(
+      screenSize.width / 2,
+      screenSize.height / 2,
+    ),
+  );
+}
   }
 }
