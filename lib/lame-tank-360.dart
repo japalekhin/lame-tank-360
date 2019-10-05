ebbba7c862b904f7ae392c6c47ad4f1f925ea2dc
Japa Alekhin Llemos <jap@alekhin.io>
2019-10-05 14:05:37 +0800

    added game class file

---
+++ lib/lame-tank-360.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+import 'package:flame/game.dart';
+
+class LameTank360 extends Game {
+  Size screenSize;
+
+  @override
+  void render(Canvas canvas) {
+    if (screenSize == null) {
+      return; // just end execution if there's no screenSize
+    }
+  }
+
+  @override
+  void update(double t) {
+    if (screenSize == null) {
+      return; // just end execution if there's no screenSize
+    }
+  }
+
+  @override
+  void resize(Size size) {
+    screenSize = size;
+  }
+}
