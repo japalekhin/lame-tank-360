9ff962eeb89ec3b3016ad307281b439c30b56190
Japa Alekhin Llemos <jap@alekhin.io>
2019-10-06 23:34:30 +0800

    added buttons to ui layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:lame_tank_360/lame-tank-360.dart';
+import 'package:lame_tank_360/widgets/button.dart';
 import 'package:lame_tank_360/widgets/joypad.dart';
 
 Future main() async {
@@ -29,6 +30,20 @@ Future main() async {
           Column(
             children: <Widget>[
               Spacer(),
+              Row(
+                children: <Widget>[
+                  SizedBox(width: 48),
+                  Button(
+                    onTap: game.onButtonTap,
+                  ),
+                  Spacer(),
+                  Button(
+                    onTap: game.onButtonTap,
+                  ),
+                  SizedBox(width: 48),
+                ],
+              ),
+              SizedBox(height: 20),
               Row(
                 children: <Widget>[
                   SizedBox(width: 48),
